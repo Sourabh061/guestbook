e461edb6ee7e737aadab8d0c779f2eb21e800637
Sourabh061 <75979495+Sourabh061@users.noreply.github.com>
2022-04-05 18:36:57 +0530

    Update binding-hack.sh

--- v2/binding-hack.sh
+++ v2/binding-hack.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-tone_analyzer="Tone Analyzer-z7"
+tone_analyzer="Natural Language Understanding-ff"
 
-B64_URL=$(ibmcloud resource service-keys --instance-name "$tone_analyzer" --output json| jq .[0].credentials.url -j | base64 -w 0)
-B64_APIKEY=$(ibmcloud resource service-keys --instance-name "$tone_analyzer" --output json| jq .[0].credentials.apikey -j | base64 -w 0)
+B64_URL=$(ibmcloud resource service-keys --instance-name "$naturallanguageunderstanding" --output json| jq .[0].credentials.url -j | base64 -w 0)
+B64_APIKEY=$(ibmcloud resource service-keys --instance-name "$naturallanguageunderstanding" --output json| jq .[0].credentials.apikey -j | base64 -w 0)
 
 cat <<EOF | oc apply -f -
 apiVersion: v1
